2139fdcc8be2a35f03959d8a747edd4bcb9740b2
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-02-28 19:53:38 -0500

    ht() function, user can quickly add .hide_tracked to a project

---
+++ ht.sh
@@ -0,0 +1,9 @@
+ht() {
+  if_not_found_append_to_file ".hide_tracked" ".gitignore"
+  read -p "Enter a new file to be tracked: " newfile
+  if_not_found_append_to_file "$newfile" ".hide_tracked"
+}
+if_not_found_append_to_file() {
+  # $1=filename, $2=pattern
+  if grep -q "$1" "$2"; then :; else echo "$1" >> $2; fi
+}
