6a9eb949df54ccf7446c9182b9ae49ce1eaa2a93
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-03-01 10:24:08 -0500

    Update ht.sh

--- ht.sh
+++ ht.sh
@@ -1,5 +1,6 @@
 ht() {
   if_not_found_append_to_file ".hide_tracked" ".gitignore"
+  if_not_found_append_to_file ".gitignore" ".hide_tracked"
   read -p "Enter a new file to be tracked: " newfile
   if_not_found_append_to_file "$newfile" ".hide_tracked"
 }
