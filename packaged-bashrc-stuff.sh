ab02ada2521ea1331f54ef7c9411db5369d5fe14
MichaelDimmitt <michaelgdimmitt@gmail.com>
2021-03-02 22:21:49 -0500

    Create packaged-bashrc-stuff.sh

---
+++ packaged-bashrc-stuff.sh
@@ -0,0 +1,22 @@
+git() {
+  if [ "$1" == "check" ]; then
+    bash <(curl -s https://raw.githubusercontent.com/MichaelDimmitt/git_check_computer/master/git_check_computer.sh)
+  elif [ "$1" == "add" ]; then
+    $(which git) "$@"
+    while IFS='' read -r LINE || [ -n "${LINE}" ]; do
+      $(which git) reset HEAD $LINE;
+    done < .hide_tracked
+  else
+    $(which git) "$@"
+  fi
+}
+ht() {
+  if_not_found_append_to_file ".hide_tracked" ".gitignore"
+  if_not_found_append_to_file ".gitignore" ".hide_tracked"
+  read -p "Enter a new file to be tracked: " newfile
+  if_not_found_append_to_file "$newfile" ".hide_tracked"
+}
+if_not_found_append_to_file() {
+  # $1=filename, $2=pattern
+  if grep -q "$1" "$2"; then :; else echo "$1" >> $2; fi
+}
